44926fbcf8324c34014c2737231fc00b9be16f1a
unknown <jurabusa2@gmail.com>
2025-05-29 16:30:52 -0500

    Autenticación en demás modulos

--- src/routes/level_routes.js
+++ src/routes/level_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const levelController = require('../controllers/level_controller');
+const authentication = require('../middlewares/authentication');
 
-router.post('/register', levelController.registerLevel);
-router.get('/list', levelController.listLevels);
-router.put('/update/:id', levelController.updateLevel);
-router.delete('/delete/:id', levelController.deleteLevel);
+router.post('/register',authentication, levelController.registerLevel);
+router.get('/list',authentication, levelController.listLevels);
+router.put('/update/:id', authentication,levelController.updateLevel);
+router.delete('/delete/:id',authentication, levelController.deleteLevel);
 
 module.exports = router;
--- src/routes/user_level_routes.js
+++ src/routes/user_level_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userLevelController = require('../controllers/user_level_controller');
+const authentication = require('../middlewares/authentication');
 
-router.post('/register', userLevelController.registerUserLevel);
-router.get('/list', userLevelController.listUsersLevels);
-router.put('/update/:id', userLevelController.updateUserLevel);
-router.delete('/delete/:id', userLevelController.deleteUserLevel);
+router.post('/register',authentication, userLevelController.registerUserLevel);
+router.get('/list',authentication, userLevelController.listUsersLevels);
+router.put('/update/:id',authentication, userLevelController.updateUserLevel);
+router.delete('/delete/:id',authentication, userLevelController.deleteUserLevel);
 
 module.exports = router;
